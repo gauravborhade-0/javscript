33ac9ce33bbfe3849c301125a455b0b590331f11
gauravborhade-0 <141417654+gauravborhade-0@users.noreply.github.com>
2024-07-01 12:17:30 +0000

    this and arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,45 @@
+// const user = {
+//     username : "hitesh",
+//     price: 999,
+
+//     welcomeMessage : function(){
+//         console.log(`${this.username},welcome to website`);
+//         console.log(this); 
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username = 'sam'
+// user.welcomeMessage()
+
+// console.log(this); 
+
+// function chai(){   //we can't use this keyword in function 
+//     let username = "hitesh"
+//     console.log(this.username)
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai = () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+// chai()
+
+// const addTwo = (num1,num2)=>{  //arrow funtion it is denoted by (=>)
+    // return num1+num2
+}
+
+// const addTwo = (num1,num2) => num1+num2 //arrow function
+
+// const addTwo = (num1,num2)=>(num1+num2) //we don't need to use curly braces in this syntax
+
+
+// const addTwo = (num1,num2) => ({name:"Gaurav"}) //we can also use object in this ways
